0a7d7d9f53eaa42ffd408d2a686e69ad503e07f0
KubangPawis <xanderfernandezperegrin@gmail.com>
2024-07-20 21:01:14 +0800

    feat(js): implement round result checking logic

--- script.js
+++ script.js
@@ -30,6 +30,20 @@ function getHumanChoice() {
     return humanPrompt;
 }
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        return "You Win! Rock beats Scissors.";
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        return "You Win! Scissors beats Paper."
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        return "You Win! Paper beats Rock.";
+    } else if (humanChoice === computerChoice) {
+        return `Draw! You both picked ${humanChoice}`;
+    } else {
+        return `You Lose! ${humanChoice} loses to ${computerChoice}`;
+    }
+}
+
 const NUMBER_OF_ROUNDS = 5;
 let humanScore = 0;
 let computerScore = 0;
@@ -37,6 +51,11 @@ let computerScore = 0;
 for (let i = 0; i < NUMBER_OF_ROUNDS; i++) {
     const computerChoice = getComputerChoice();
     const humanChoice = getHumanChoice();
-    console.log(`Computer - Round #${i + 1}: ` + computerChoice);
-    console.log(`Human - Round #${i + 1}: ` + humanChoice);
+
+    console.log(`ROUND #${i + 1}`);
+    console.log("Computer: " + computerChoice);
+    console.log("Human: " + humanChoice);
+
+    const roundResult = playRound(humanChoice, computerChoice);
+    console.log(roundResult);
 }
