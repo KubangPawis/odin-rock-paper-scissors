f898a4dc38ce6108dc06598c1bb1830ff6c0b35a
KubangPawis <xanderfernandezperegrin@gmail.com>
2024-07-20 20:43:04 +0800

    feat(js): add human choice getter

--- script.js
+++ script.js
@@ -15,9 +15,26 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    const choiceArr = ["Rock", "Paper", "Scissors"];
+    let humanPrompt;
+
+    while (!choiceArr.includes(humanPrompt)) {
+        humanPrompt = prompt("ROCK, PAPER, or SCISSORS?");
+        humanPrompt = humanPrompt.charAt(0).toUpperCase() + humanPrompt.slice(1).toLowerCase();
+
+        if (!choiceArr.includes(humanPrompt)) {
+            alert("Invalid answer! Please try again...");
+        }
+    }
+    return humanPrompt;
+}
+
 const NUMBER_OF_ROUNDS = 5;
 
 for (let i = 0; i < NUMBER_OF_ROUNDS; i++) {
     const computerChoice = getComputerChoice();
-    console.log(`Round #${i + 1}: ` + computerChoice);
+    const humanChoice = getHumanChoice();
+    console.log(`Computer - Round #${i + 1}: ` + computerChoice);
+    console.log(`Human - Round #${i + 1}: ` + humanChoice);
 }
